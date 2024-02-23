91b52e100bcd58a4c2ed2bbe52615475f14b04b3
Cleiston Amaral <c.alexandresouza@yahoo.com>
2024-02-23 18:42:04 -0300

    Challenge 2 lewagon day 27

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -7,3 +8,13 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts 'Creating restaurants...'
+
+5.times do
+  Restaurant.create({
+    name: Faker::Restaurant.name,
+    address: Faker::Games::Witcher.location,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: Faker::Restaurant.type
+  })
+end
